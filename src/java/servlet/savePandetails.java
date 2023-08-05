1678450f40a07449e2779d7eb07ed1a7a94ef4e1
tuhin <tuhin@tuhin.com>
2023-08-05 18:23:47 +0530

    5th Aug 2023, 6:24 P.M

--- src/java/servlet/savePandetails.java
+++ src/java/servlet/savePandetails.java
@@ -14,9 +14,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
 import org.json.JSONObject;
 import java.sql.*;
+import java.util.Base64;
 import org.json.JSONArray;
+import static servlet.saveLiveImage.base64StringToByteArray;
 
 /**
  *
@@ -48,12 +51,15 @@ public class savePandetails extends HttpServlet {
             cs.setString(3, emailId);
             cs.setInt(4, relativeCode);
 
+            savePanProofImage(jsonBody);
+
             // Execute the stored procedure
             rs = cs.executeQuery();
 
             JSONArray jsonArray = DBUtil.resultSetToJsonArray(rs);
-            
-            System.err.println("jsResponse: "+jsonArray);
+
+            System.err.println("jsResponse: " + jsonArray);
+
             // You can now send the JSONArray in the response as JSON data
             resp.setContentType("application/json");
             resp.setStatus(HttpServletResponse.SC_OK);
@@ -67,6 +73,57 @@ public class savePandetails extends HttpServlet {
             resp.getWriter().write("An internal server error occurred.");
             return;
         }
+    }
+
+    private void savePanProofImage(JSONObject jsonBody) throws SQLException {
+        String panProof = jsonBody.getString("panProof");
+
+        String panno = jsonBody.getString("panNo");
+        int catid = jsonBody.getInt("catid");
+        String imageName = jsonBody.getString("imageName");
+        int imgWidth = jsonBody.getInt("imgWidth");
+        int imgHeight = jsonBody.getInt("imgHeight");
+        int pageCount = jsonBody.getInt("pageCount");
+        String proofName = jsonBody.getString("proofName");
+        int Flg = jsonBody.getInt("Flg");
+        String RejChkValue = jsonBody.getString("RejChkValue");
+        String RejText = jsonBody.getString("RejText");
+        int Count = jsonBody.getInt("Count");
+        String RegNo = jsonBody.getString("RegNo");
+        String RegExpDate = jsonBody.getString("RegExpDate");
+        int filesize = jsonBody.getInt("filesize");
+
+        byte[] imgArray = base64StringToByteArray(panProof);
+
+        Connection conn2 = DBUtil.getConnection();
+
+        CallableStatement cs2 = conn2.prepareCall(DBConstraints.EKYC_SAVE_LIVE_IMAGE);
+
+        cs2.setBytes(1, imgArray);
+        cs2.setString(2, panno);
+        cs2.setInt(3, catid);
+        cs2.setString(4, imageName);
+        cs2.setInt(5, imgWidth);
+        cs2.setInt(6, imgHeight);
+        cs2.setInt(7, pageCount);
+        cs2.setString(8, proofName);
+        cs2.setInt(9, Flg);
+        cs2.setString(10, RejChkValue);
+        cs2.setString(11, RejText);
+        cs2.setInt(12, Count);
+        cs2.setString(13, RegNo);
+        cs2.setString(14, RegExpDate);
+        cs2.setInt(15, filesize);
+
+        // Execute the stored procedure
+        cs2.execute();
+    }
+    
+    public static byte[] base64StringToByteArray(String base64String) {
+        // Decode the Base64 string to byte array
+//        byte[] byteArray = Base64.getDecoder().decode(base64String);
+        byte[] byteArray = Base64.getDecoder().decode(base64String.getBytes(StandardCharsets.UTF_8));
+        return byteArray;
     }
 
 }
